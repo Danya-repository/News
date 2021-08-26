3aaed79e1f8c6ff6cad885ed25a370b0b7742f61
Danya Kashapov <d.kashapov@napoleonit.ru>
2021-08-26 23:49:09 +0300

    maked header and start make main

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import '../../node_modules/focus-visible/dist/focus-visible';
+import '../index.html';
+import '../sass/main.sass';
+
+// import $ from 'jquery';
+
+
+
+// const a = 5;
+//
+// $('body').css('color', 'red');
